125b391e7b1fa76d8253569bd7c8185ad8eee6ae
Batuhan Uzun <batuhanuzun2356@gmail.com>
2024-07-18 05:44:47 +0300

    IfElse Practice

--- IfElsePractice.cs
+++ IfElsePractice.cs
@@ -1,11 +1,11 @@
 ﻿Console.Write("Lütfen bir sayı giriniz: ");
-int sayi = Convert.ToInt32(Console.ReadLine());
+int number = Convert.ToInt32(Console.ReadLine());
 
 //Girilen sayının 10'dan büyük mü küçük mü veya eşit mi kontrolünün yapıldığı kod bloğu
-if (sayi > 10) Console.WriteLine("Girilen sayi 10'dan büyüktür");
-else if (sayi == 10) Console.WriteLine("Girilen sayi 10'a esittir");
+if (number > 10) Console.WriteLine("Girilen sayi 10'dan büyüktür");
+else if (number == 10) Console.WriteLine("Girilen sayi 10'a esittir");
 else Console.WriteLine("Girilen sayi 10'dan kücüktür");
 
 //Girilen sayının tek mi çift mi olduğunun kontrolünün yapıldığı kod bloğu
-if (sayi % 2 == 0) Console.WriteLine("Girilen sayi cifttir");
+if (number % 2 == 0) Console.WriteLine("Girilen sayi cifttir");
 else Console.WriteLine("Girilen sayi tektir");
